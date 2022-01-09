4648eaa63648875d500320a0ae9ffe360a770926
AlexMkGitHub <mkalex77@gmail.com>
2022-01-09 21:40:14 +0700

    Практическая работа 2. Telnet server. Промежуточный вариант 1.

--- TelnetNeoTerminal/src/nio/NioEchoServer.java
+++ TelnetNeoTerminal/src/nio/NioEchoServer.java
@@ -1,5 +1,6 @@
 package nio;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -8,7 +9,12 @@ import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 public class NioEchoServer {
@@ -20,7 +26,7 @@ public class NioEchoServer {
      * cat file_name - распечатать содержание файла на экран
      * mkdir dir_name - создать директорию в текущей
      * touch file_name - создать пустой файл в текущей директории
-     * */
+     */
 
     private ServerSocketChannel serverChannel;
     private Selector selector;
@@ -71,9 +77,88 @@ public class NioEchoServer {
             }
             buf.clear();
         }
-        // process(s)
+        String command = s.toString();
+
+        if (command.startsWith("ls\r")) {
+            filesList(channel);
+            return;
+        }
+        if (command.startsWith("cd ")) {
+            moveToDirectory(channel, command);
+            return;
+        }
+        if (command.startsWith("cat ")) {
+            printFileContent(command);
+            return;
+        }
+        if (command.startsWith("mkdir ")) {
+            createDirectory(command);
+            return;
+        }
+        if (command.startsWith("touch ")) {
+            emptyFile(command);
+            return;
+        }
         System.out.println("Received: " + s);
         channel.write(ByteBuffer.wrap(s.toString().getBytes(StandardCharsets.UTF_8)));
+    }
+
+    private void filesList(SocketChannel channel) throws IOException {
+        System.out.println("Получена команда: ls");
+        String currentDir = System.getProperty("user.dir");
+        String fileName = "..\n\r";
+        File dir = new File(currentDir);
+        File[] arrFiles = dir.listFiles();
+        List<File> lst = Arrays.asList(arrFiles);
+        channel.write(ByteBuffer.wrap(fileName.getBytes(StandardCharsets.UTF_8)));
+        for (File file : lst) {
+            fileName = file.getName() + "\n\r";
+            channel.write(ByteBuffer.wrap(fileName.getBytes(StandardCharsets.UTF_8)));
+            System.out.println(file.getName());
+        }
+    }
+
+    //перегрузка метода filesList(), для использования в других методах
+    private void filesList(SocketChannel channel, Path command) throws IOException {
+        System.out.println("Получена команда: ls");
+        String currentDir = command.toString();
+        String fileName = "..\n\r";
+        File dir = new File(currentDir);
+        File[] arrFiles = dir.listFiles();
+        List<File> lst = Arrays.asList(arrFiles);
+        channel.write(ByteBuffer.wrap(fileName.getBytes(StandardCharsets.UTF_8)));
+        for (File file : lst) {
+            fileName = file.getName() + "\n\r";
+            channel.write(ByteBuffer.wrap(fileName.getBytes(StandardCharsets.UTF_8)));
+            System.out.println(file.getName());
+        }
+    }
+
+    private void moveToDirectory(SocketChannel channel, String command) throws IOException {
+        System.out.println("Получена команда: cd");
+        String[] token = command.split("\\s+");
+        if (token.length < 2) {
+            return;
+        }
+        Path dir = Paths.get(token[1]);
+        if (Files.exists(dir)) {
+            filesList(channel, dir);
+            return;
+        } else {
+            System.out.println("НЕТ ТАКОЙ ПАПКИ");
+        }
+    }
+
+    private void printFileContent(String command) {
+        System.out.println("Получена команда: cat");
+    }
+
+    private void createDirectory(String command) {
+        System.out.println("Получена команда: mkdir");
+    }
+
+    private void emptyFile(String command) {
+        System.out.println("Получена команда: touch");
     }
 
     private void handleAccept() throws IOException {
